29cc9d1fd59d614883cedaafdb8b572773e29478
ggbarreiros <ggbarreiros@hotmail.com>
2019-10-03 19:29:08 -0300

    Scripts adicionados
    Player Class e World Class

---
+++ Lixoso/Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass : MonoBehaviour
+{
+    public int inventory; //make an array, around 10 slots max
+    public float grabRange;
+    public int lives;
+    public int score;
+}
---
+++ Lixoso/Assets/Scripts/WorldClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldClass : MonoBehaviour
+{
+    public float timer;
+    public int currentTrashAmountByArea; //also an array, 4 positions
+    public int maxTrashAmountByArea;
+    public float trashSpawnRate;
+}
