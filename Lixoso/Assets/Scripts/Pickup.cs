59c41cb853fb8706aa457a9932245dffda75fedb
aetherboundEsotericist <44498928+aetherboundEsotericist@users.noreply.github.com>
2019-10-07 16:19:06 -0300

    07.10.19
    Adicionado script de coleta de lixo

---
+++ Lixoso/Assets/Scripts/Pickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Pickup : MonoBehaviour
+{
+    public float rayLength;
+    public LayerMask whatToHit; //LayerMask onde apenas o lixo deve ser selecionado
+    public Camera playerCamera;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, rayLength, whatToHit)) //se acertar um lixo
+            {
+                ChangeTrashCount(hit.collider.tag);
+                Debug.Log(hit.collider.name);
+
+            }
+        }
+    }
+
+    public void ChangeTrashCount(string trashType)
+    {
+
+    }
+}
+//780 1280
